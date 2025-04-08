d5e9cc4ec551745b80e81e7de5e5b8469b6b67ec
giordanog1 <ultimategiordy@gmail.com>
2025-04-08 15:33:21 -0400

    updated logo, text, and homepage

--- home.php
+++ home.php
@@ -7,26 +7,38 @@
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
-  <nav class="navbar">
-    <div class="navbar__container">
-       <img src="images/roarybuddylogo.png" id="navbar__logo">
-       <p id="appTitle">RoaryBuddy</p>
-       <div class="navbar__toggle" id="mobile-menu">
-        <span class="bar"></span>
-        <span class="bar"></span>
-        <span class="bar"></span>
-       </div>
-       <ul class="navbar__menu">
-        <li class="navbar__item">
-          <a href="home.html" class="navbar__links">Home</a>
-        </li>
-        <li class="navbar__btn">
-          <a href="login.php" class="loginButton">Login</a>
-        </li>
-       </ul>
-    </div>
 
-  </nav>
-  <script src="app.js"></script>
+    <!-- Navigation Bar -->
+    <nav class="navbar">
+      <div class="navbar__container">
+       <div class="navbar__brand">
+          <img src="images/roarybuddylogo.png" id="navbar__logo">
+          <img src="images/roarybuddytext.png" id="appTitle">
+        </div>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="home.php" class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__btn">
+            <a href="login.php" class="loginButton">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Hero Section -->
+    <section class="hero">
+      <div class="hero__content">
+        <h1>Welcome to <img src="images/roarybuddytext.png"></h1>
+        <a href="login.php" class="loginButton">Get Started</a>
+      </div>
+    </section>
+
+    <script src="app.js"></script>
   </body>
 </html>
