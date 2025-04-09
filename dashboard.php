0a9302a11442ba4da4c3c69b25c25e94f877bd63
giordanog1 <ultimategiordy@gmail.com>
2025-04-08 22:35:27 -0400

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,31 @@
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
+        <!-- Navigation Bar -->
+      <nav class="navbar">
+      <div class="navbar__container">
+       <div class="navbar__brand">
+        <a href="home.php">
+          <img src="images/roarybuddylogo.png" id="navbar__logo">
+          <img src="images/roarybuddytext.png" id="appTitle">
+        </a>
+        </div>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <li class="navbar__item">
+            <a href="home.php" class="navbar__links">Home</a>
+          </li>
+          <li class="navbar__btn">
+            <a href="#" class="loginButton">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
   Hello Welcome to the Dashboard!
 </body>
   </nav>
