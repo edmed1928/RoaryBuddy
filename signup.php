74a276166828e1665a004f97a138587db11376ab
giordanog1 <ultimategiordy@gmail.com>
2025-04-13 16:21:43 -0400

    updated login/register page design

--- signup.php
+++ signup.php
@@ -8,27 +8,25 @@
 </head>
 <body>
     <div class="signup-container">
-        <h2>Sign Up for RoaryBuddy</h2>
+        <div class="signup-header">
+            <img src="images/signuptext.png" alt="Sign Up" class="signup-image">
+        </div>
         <form action="includes/formhandler_signup.inc.php" method="POST">
             <label for="name">Full Name:</label>
             <input type="text" id="name" name="name" required>
-
-            
+           
             <label for="username">Username:</label>
             <input type="text" id="username" name="username" required>
-
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" required>
-            
+           
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
-
             <label for="role">Are you registering as a student or admin?</label>
       <select id="role" name="role">
         <option value="student">Student</option>
         <option value="admin">Admin</option>
       </select>
-
             <button type="submit">Sign Up</button>
         </form>
         <p>Already have an account? <a href="login.php">Login here</a></p>
